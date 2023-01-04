d06dc9fde354abb3930dc6a8a2e4667a91d41ce1
Kenneth <kenneth1.kodehode@gmail.com>
2023-01-04 14:02:44 +0100

    t

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
     <>
       <div className="card w-96 bg-base-100 shadow-xl flex m-auto mt-20">
         <figure>
-          <div className="bg-[url('https://freeimage.host/i/HAWL9Jn')]" />
+          <img className="[url('https://freeimage.host/i/HAWL9Jn')]" />
         </figure>
         <div className="card-body h-20">
           <h1 className="card-title justify-center text-2xl font-sans">
@@ -30,6 +30,7 @@ function Contact() {
           <a href="https://www.google.com/maps/place/Nygata+16B,+3950+Brevik/@59.051801,9.7042902,17z/data=!3m1!4b1!4m5!3m4!1s0x4646e107e49a5eeb:0xba42035f30e88ded!8m2!3d59.0517983!4d9.7064842">
             <img
               src="/src/images/home.png"
+              alt=""
               className="w-20 inline hover:scale-125 mb-6"
             ></img>
             <h4 className="justify-center inline ml-2 font-sans">
