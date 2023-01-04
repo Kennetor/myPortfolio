175513241b234dde016a4f3c532874f8acd8f090
Your Name <kenneth1.kodehode@gmail.com>
2023-01-04 23:03:50 +0100

    content tips

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,9 +5,25 @@ import React from "react";
 function About() {
   return (
     <>
-      <h1>Jao</h1>
+      <h1>About Me</h1>
     </>
   );
 }
 
 export default About;
+
+/*
+The about page on a portfolio website is an opportunity to give visitors a more personal and in-depth look at who you are and what you do. Here are some things you might consider including on your about page:
+
+A personal statement or bio: This could be a short paragraph or a couple of sentences introducing yourself and outlining your professional background and goals.
+
+A list of your skills and expertise: This could include technical skills, such as programming languages and frameworks, as well as soft skills, such as communication and problem-solving.
+
+Your education and work experience: This could include a summary of your education (e.g., degrees or certifications) and a list of your previous jobs or internships.
+
+Personal interests and hobbies: While not directly related to your professional work, including a bit of personal information can help visitors get to know you better and make a connection with you.
+
+A photo: Including a photo of yourself can help visitors put a face to the name and make your website feel more personal.
+
+Remember, the about page is an opportunity to showcase your personality and values as well as your professional skills and experience. Make sure to keep it interesting and engaging, and be sure to proofread it carefully to ensure that it is error-free.
+*/
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,8 +1,11 @@
 function Skills() {
   return (
     <>
-      <h1>Joakim</h1>
+      <h1>Showcase</h1>
     </>
   );
 }
 export default Skills;
+/* 
+This section should showcase the developer's work, including live demos and links to the code. It could be organized by project or by skill area (e.g., web design, JavaScript development).
+*/
