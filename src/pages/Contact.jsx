85833cf224120af232f3b38f70494fe0603c4df1
Kenneth <kenneth1.kodehode@gmail.com>
2023-01-04 14:04:39 +0100

    e

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -29,7 +29,7 @@ function Contact() {
         <div className="ml-10">
           <a href="https://www.google.com/maps/place/Nygata+16B,+3950+Brevik/@59.051801,9.7042902,17z/data=!3m1!4b1!4m5!3m4!1s0x4646e107e49a5eeb:0xba42035f30e88ded!8m2!3d59.0517983!4d9.7064842">
             <img
-              src="/src/images/home.png"
+              src="../images/home.png"
               alt=""
               className="w-20 inline hover:scale-125 mb-6"
             ></img>
